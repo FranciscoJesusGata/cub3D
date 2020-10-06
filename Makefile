f8b8255eaee01f047258496907c0faf8a5a851aa
fgata-va <fgata-va@student.42madrid.com>
2020-10-06 11:23:49 +0200

    File allocated with no memory leaks

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: fgata-va <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/29 12:43:45 by fgata-va          #+#    #+#              #
-#    Updated: 2020/10/02 13:11:38 by fgata-va         ###   ########.fr        #
+#    Updated: 2020/10/06 10:39:41 by fgata-va         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = gcc
 
-CFLAGS += -Wall -Werror -Wextra -I .
+CFLAGS += -Wall -Werror -Wextra -I . -g
 
 GNL = lib/GNL/get_next_line.c lib/GNL/get_next_line_utils.c
 
@@ -20,11 +20,11 @@ SRC = cube3d.c
 
 NAME = cube3d
 
-LIBFT = -L lib/libftprintf/ -lft
+LIBFT = -L lib/libftprintf -lftprintf
 
 MLX = -L . -lmlx -framework OpenGL -framework Appkit
 
-INCLUDE = -I lib/GNL/ -I lib/libftprintf/ -I lib/mlx/
+all: $(NAME)
 
 libft:
 	@$(MAKE) -C lib/libftprintf all
@@ -33,10 +33,11 @@ mlx:
 	@$(MAKE) -C lib/mlx all
 	mv lib/mlx/libmlx.dylib .
 
-all: $(NAME)
+$(NAME): libft mlx
+	$(CC) $(CFLAGS) $(SRC) $(GNL) $(LIBFT) $(MLX) -o $(NAME)
 
-$(NAME):
-	$(CC) $(CFLAGS) $(INCLUDE) $(SRC) $(GNL) $(LIBFT) $(MLX) -o $(NAME)
+test: libft
+	$(CC) $(CFLAGS) $(SRC) $(GNL) $(LIBFT)
 
 clean:
 	@make -C lib/libftprintf fclean
--- cube3d.c
+++ cube3d.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 11:40:34 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/10/02 19:27:20 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/10/06 10:24:06 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,42 @@
 void		*ft_resize(void *p, size_t oldlen, size_t newlen)
 {
 	void	*new;
-	size_t	len;
 
+	if (!p)
+		return (malloc(newlen));
+	if(newlen <= oldlen)
+		return (p);
 	new = malloc(newlen);
-	if(oldlen < newlen)
-		len = oldlen;
-	else
-		len = newlen;
-	if (p && new)
-		ft_memcpy(new, p, len);
-	if (new)
-		free(p);
-	return (new);
+	ft_memcpy(new, p, newlen);
+	free(p);
+	return(new);
 }
 
 char		**ft_newline(char **file, char *line, size_t size)
 {
-	char *new_matrix;
+	char **new_matrix;
 
-	new_matrix = ft_resize(file, size, (sizeof(char *) + size));
-	((char **)new_matrix)[size] = line;
-	line = NULL;
-	return ((char **)new_matrix);
+	new_matrix = ft_resize(file, size * sizeof(char *), (size + 1) * sizeof(char *));
+	new_matrix[size] = ft_strdup(line);
+	free(line);
+	return (new_matrix);
 }
 
-char		**ft_read_map(int fd)
+void	*ft_read_map(int fd)
 {
 	char	*line;
 	int		i;
 	char	**file;
 
+	file = NULL;
 	i = 0;
 	while((get_next_line(fd, &line)) == 1)
 	{
-		file = ft_newline(file, line, (i * sizeof(char *)));
+		file = ft_newline(file, line, i);
 		i++;
 	}
+	free(line);
+	file[i] = NULL;
 	return(file);
 }
 
@@ -60,19 +60,23 @@ int			cube3d(char *path)
 	char	**file;
 	int		i;
 
+	file = NULL;
 	if(!(fd = open(path, O_RDONLY)))
 	{
 		write(1, "Error\n", 6);
-		return(0);
+		return(1);
 	}
 	file = ft_read_map(fd);
 	i = 0;
 	while (file[i])
 	{
 		ft_printf("%s\n", file[i]);
+		free(file[i]);
 		i++;
 	}
-	return (1);
+	free(file[i]);
+	free(file);
+	return (0);
 }
 
 int			main(void)
