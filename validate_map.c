a6a47f66f6214f2d59ea16db0d438dee332b67bc
Francisco Jesús Gata <fgata-va@student.42madrid.com>
2020-11-09 19:11:28 +0100

    Map validation in process...

--- validate_map.c
+++ validate_map.c
@@ -6,71 +6,89 @@
 /*   By: fgata-va <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 11:26:02 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/10/28 10:55:05 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/11/09 18:25:13 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void		ft_check_elements(t_map *data)
+int			ft_check_player(char **map)
 {
 	int		i;
 	int		j;
-	int		player_fnd;
-	char	**mapa;
+	int		fnd;
 
+	fnd = 0;
 	i = 0;
-	player_fnd = 0;
-	mapa = data->map_matrix;
-	while(mapa[i])
+	while (map[i])
 	{
 		j = 0;
-		while (mapa[i][j])
+		while (map[i][j])
 		{
-			if (!(ft_strchr("012NSWE ", mapa[i][j])))
+			if (ft_strchr("NSWE", map[i][j]))
 			{
-				data->valid = 0;
-				break ;
-			}
-			else if (ft_strchr("NSWE", mapa[i][j]))
-			{
-				if (player_fnd == 1)
-				{
-					data->valid = 0;
-					break ;
-				}
-				data->valid = 1;
+				if (fnd == 1)
+					return (0);
+				fnd = 1;
 			}
 			j++;
 		}
-		if (data->valid == 0)
-			break ;
 		i++;
 	}
-	if (player_fnd == 0)
-	{
-		data->valid = 0;
-		return ;
-	}
+	if (fnd == 0)
+		return (0);
+	return (1);
 }
 
 
-void		map_validator(char **map, int x, int y, t_map *data)
+int		ft_check_elements(t_map *data, t_cub_flags *flags)
 {
-	if (data->valid == 0)
-		return ;
-	if (map[y][x] != 1 && map[y][x] != 3)
+	int		i;
+	int		j;
+	char	**map;
+
+	i = 0;
+	map = data->map_matrix;
+	flags->valid_map = 1;
+	while(map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (!(ft_strchr("012NSWE ", map[i][j])))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (ft_check_player(map))
+		return (0);
+}
+
+
+int		map_validator(char **map, int x, int y, t_map *data)
+{
+	int	valid;
+
+	valid = 1;
+	if (map[y][x] != 1 && map[y][x] != 3 && valid == 1)
 	{
 		if (x == ft_strlen(map[y]) || y == data->max_y || x == 0 || y == 0)
 		{
-			data->valid = 0;
-			return ;
+			return (0);
 		}
-		map_validator(map, x + 1, y, data);
+		map[y][x] = 3;
+		valid =map_validator(map, x + 1, y, data);
 		if (x < ft_strlen(map[y - 1]))
-			map_validator(map, x, y - 1, data);
-		map_validator(map, x - 1, y, data);
+			valid = map_validator(map, x, y - 1, data);
+		valid = map_validator(map, x - 1, y, data);
 		if (x < ft_strlen(map[y + 1]))
-		map_validator(map, x, y + 1, data);
+			valid =map_validator(map, x, y + 1, data);
 	}
+	return (valid);
+}
+
+void		ft_valid_map(t_map *data, t_cub_flags *flags)
+{
+	
 }
