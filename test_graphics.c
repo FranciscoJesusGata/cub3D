4931e05eb27255864208fa94b9d03e191dd9ee6a
Francisco Jesús Gata <fgata-va@student.42madrid.com>
2020-12-02 21:11:39 +0100

    Still learning

--- test_graphics.c
+++ test_graphics.c
@@ -1,4 +1,5 @@
-#include "mlx.h"
+#include "lib/mlx_linux/mlx.h"
+#include <stdio.h>
 
 typedef struct	s_data {
 void	*img;
@@ -8,12 +9,46 @@ int		line_length;
 int		endian;
 }				t_data;
 
-void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef struct	s_vars {
+void	*mlx;
+void	*win;
+}				t_vars;
+
+
+void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    char    *dst;
+    char		*dst;
 
     dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
+}
+
+void			ft_print_square(t_vars *vars, int side, t_data *img, int color, int width, int height)
+{
+	int		i;
+	int		j;
+
+	img->img = mlx_new_image(vars->mlx, side, side);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length,
+		&img->endian);
+	i = 0;
+	while (i < side)
+	{
+		j = 0;
+		while (j < side)
+		{
+			my_mlx_pixel_put(img, i, j, color);
+			j++;
+		}
+		i++;
+	}
+	mlx_put_image_to_window(vars->mlx, vars->win, img->img, (width - i) / 2, (height - j) / 2);
+}
+
+int		ft_print_key(int keycode, t_vars *vars)
+{
+	printf("Key %d pressed\n", keycode);
+	return (0);
 }
 
 int			main(void)
@@ -21,24 +56,11 @@ int			main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	int		i = 0;
-	int		j;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 500, 1000);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-		&img.endian);
-	while(i < 500)
-	{
-		j = 0;
-		while (j < 1000)
-		{
-			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
-			j++;
-		}
-		i++;
-	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, (1920 - i) / 2, (1080 - j) / 2);
-	mlx_loop(mlx);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1000, 800, "Hello world!");
+	ft_print_square(&vars, 500, &img, 0x00FF0000, 1000, 800);
+	mlx_key_hook(vars.win, ft_print_key, &vars);
+	mlx_loop(vars.mlx);
 }
