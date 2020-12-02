e35b30d6155c6de6f2d93deee2af02c2cc9418d6
Francisco jesus Gata valdes <fgata-va@c1r3s3.42madrid.com>
2020-12-02 13:21:48 +0100

    learning how to use the MiniLibX

--- graphics_1.c
+++ graphics_1.c
@@ -6,16 +6,38 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 12:50:03 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/12/01 13:00:11 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/12/02 10:23:39 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void		ft_pixel_put(char *addr, int offset, int color)
+{
+	char	*dst;
+
+	dst = addr + offset;
+	*dst = color;
+}
+
 void		ft_start_screen(t_map *data, t_tex *tex)
 {
+	void	*img;
+	char	*addr;
+	int		bpp = 0;
+	int		line_len = 0;
+	int		endian;
+	int		offset;
+	int		x = 5;
+	int		y = 5;
+
 	ft_print_data(data, tex);
 	data->window = mlx_new_window(data->mlx_ptr, data->resolution[0],
 								data->resolution[1], "Cub3D");
+	img = mlx_new_image(data->mlx_ptr, data->resolution[0], data->resolution[1]);
+	addr = mlx_get_data_addr(img, &bpp, &line_len, &endian);
+	offset = y * line_len + x * (bpp / 8);
+	ft_pixel_put(addr, offset, 0x00FF0000);
+	mlx_put_image_to_window(data->mlx_ptr, data->window, img, 0, 0);
 	mlx_loop(data->mlx_ptr);
 }
---
+++ test_graphics.c
@@ -0,0 +1,44 @@
+#include "mlx.h"
+
+typedef struct	s_data {
+void	*img;
+char	*addr;
+int		bits_per_pixel;
+int		line_length;
+int		endian;
+}				t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
+
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+int			main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	int		i = 0;
+	int		j;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 500, 1000);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+		&img.endian);
+	while(i < 500)
+	{
+		j = 0;
+		while (j < 1000)
+		{
+			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+			j++;
+		}
+		i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, (1920 - i) / 2, (1080 - j) / 2);
+	mlx_loop(mlx);
+}
