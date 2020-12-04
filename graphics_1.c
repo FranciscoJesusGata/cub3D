864fc7737964b7b2156606941cbe97ecd37e4adf
Francisco jesus Gata valdes <fgata-va@c1r2s2.42madrid.com>
2020-12-04 13:21:15 +0100

    trying to do a circle that travels across the screen

--- graphics_1.c
+++ graphics_1.c
@@ -6,38 +6,34 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 12:50:03 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/12/02 10:23:39 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/12/04 09:49:13 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void		ft_pixel_put(char *addr, int offset, int color)
+int			ft_key_control(int key, t_map *data)
 {
-	char	*dst;
+	if (key == 53)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->window);
+		exit(0);
+	}
+	return (0);
+}
 
-	dst = addr + offset;
-	*dst = color;
+int			ft_window_closed(void)
+{
+	exit(0);
+	return(0);
 }
 
 void		ft_start_screen(t_map *data, t_tex *tex)
 {
-	void	*img;
-	char	*addr;
-	int		bpp = 0;
-	int		line_len = 0;
-	int		endian;
-	int		offset;
-	int		x = 5;
-	int		y = 5;
-
 	ft_print_data(data, tex);
 	data->window = mlx_new_window(data->mlx_ptr, data->resolution[0],
 								data->resolution[1], "Cub3D");
-	img = mlx_new_image(data->mlx_ptr, data->resolution[0], data->resolution[1]);
-	addr = mlx_get_data_addr(img, &bpp, &line_len, &endian);
-	offset = y * line_len + x * (bpp / 8);
-	ft_pixel_put(addr, offset, 0x00FF0000);
-	mlx_put_image_to_window(data->mlx_ptr, data->window, img, 0, 0);
+	mlx_key_hook(data->window, ft_key_control, data);
+	mlx_hook(data->window, 17, 0L,(int (*)())exit, 0);
 	mlx_loop(data->mlx_ptr);
 }
--- test_graphics.c
+++ test_graphics.c
@@ -8,23 +8,26 @@ char	*addr;
 int		bits_per_pixel;
 int		line_length;
 int		endian;
+int		x;
+int		y;
 }				t_data;
 
 typedef struct	s_vars {
 void	*mlx;
 void	*win;
+t_data	*img;
 }				t_vars;
 
 
-void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void		my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    char		*dst;
+    char	*dst;
 
     dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
 }
 
-void			ft_print_square(t_vars *vars, int side, t_data *img, int color, int width, int height)
+void		ft_print_square(t_vars *vars, int side, t_data *img, int color, int width, int height)
 {
 	int		i;
 	int		j;
@@ -46,7 +49,48 @@ void			ft_print_square(t_vars *vars, int side, t_data *img, int color, int width
 	mlx_put_image_to_window(vars->mlx, vars->win, img->img, (width - i) / 2, (height - j) / 2);
 }
 
-int		ft_print_key(int keycode, t_vars *vars)
+int			square(int n)
+{
+	return (n * n);
+}
+
+void		ft_print_circle(t_vars *vars, int color, int a, int b)
+{
+	int x;
+	int y;
+	int	d;
+	int	r;
+	t_data *img;
+
+	img = vars->img;
+	r = 10;
+	d = r * 2;
+	img->img = mlx_new_image(vars->mlx, 1000, 800);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length,
+		&img->endian);
+	y = b - r;
+	while (y >= b - r && y <= d)
+	{
+		x = a - r;
+		while (x >= a - r && x <= d)
+		{
+			if (square(x - a) + square(y - r) <= (r * r))
+				my_mlx_pixel_put(img, x, y, color);
+			x++;
+		}
+		y++;
+	}
+}
+
+void		ft_move(t_vars *vars)
+{
+	mlx_destroy_image(vars->mlx, vars->img->img);
+	ft_print_circle(vars, 0x000000FF, vars->img->x, vars->img->y);
+	printf("circle is in %d,%d\n",vars->img->x,vars->img->y);
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img->img, vars->img->x,vars->img->y);
+}
+
+int			ft_control_key(int keycode, t_vars *vars)
 {
 	printf("Key %d pressed\n", keycode);
 	if (keycode == 53)
@@ -54,17 +98,43 @@ int		ft_print_key(int keycode, t_vars *vars)
 		mlx_destroy_window(vars->mlx, vars->win);
 		exit(0);
 	}
+	/* w = 13
+	** a = 0
+	** s = 1
+	** d = 2
+	*/
+	else if (keycode == 13 && vars->img->y > 0)
+	{
+		vars->img->y -= 10;
+		ft_move(vars);
+		
+	}
+	else if (keycode == 1 && vars->img->y < 800)
+	{
+		vars->img->y += 10;
+		ft_move(vars);
+	}
+	else if (keycode == 0 && vars->img->x > 0)
+	{
+		vars->img->x -= 10;
+		ft_move(vars);
+	}
+	else if (keycode == 2 && vars->img->x < 800)
+	{
+		vars->img->x += 10;
+		ft_move(vars);
+	}
 	return (0);
 }
 
-int		ft_mouse_pos(int x, int y, t_vars *vars)
+int			ft_mouse_pos(int x, int y, t_vars *vars)
 {
 	if (x >= 0 && x <= 1000 && y >= 0 && y <= 800)
 		printf("Mouse is in x = %d, y = %d\n", x, y);
 	return (0);
 }
 
-int		ft_window_closed(void)
+int			ft_window_closed(void)
 {
 	exit(0);
 }
@@ -78,9 +148,14 @@ int			main(void)
 
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, 1000, 800, "Hello world!");
-	ft_print_square(&vars, 500, &img, 0x00FF0000, 1000, 800);
-	mlx_key_hook(vars.win, ft_print_key, &vars);
+	img.x = (1000 / 2) - 10;
+	img.y = (800 / 2) - 10;
+	vars.img = &img;
+	//ft_print_square(&vars, 500, &img, 0x00FF0000, 1000, 800);
+	ft_print_circle(&vars, 0x000000FF, img.x, img.y);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	mlx_hook(vars.win, 6, (1L<<6), ft_mouse_pos, &vars);
 	mlx_hook(vars.win, 17, 0L, ft_window_closed, 0);
+	mlx_key_hook(vars.win, ft_control_key, &vars);
 	mlx_loop(vars.mlx);
 }
